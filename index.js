7291154422178038c89ecf9f97f203bff39cb81e
Garrett <gcamoin5@gmail.com>
2023-01-05 14:31:50 -0500

    commit tree 97720209f226081af19deea8d4b25765fff63242

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(Name) {
+    cats.push(Name);
+}
+
+function destructivelyPrependCat(Name) {
+    cats.unshift(Name);
+}
+
+function destructivelyRemoveLastCat(Name) {
+    cats.pop(Name);
+}
+
+function destructivelyRemoveFirstCat(Name) {
+    cats.shift(Name);
+}
+
+
+function appendCat(Name) {
+    const cats_1 = [...cats, Name]
+    return cats_1
+    }
+
+function prependCat(Name) {
+    const cats_2 = [Name, ...cats]
+    return cats_2
+}
+
+function removeLastCat(Name) {
+    const cats_3 = cats.slice(0, cats.length - 1)
+    return cats_3
+}
+
+function removeFirstCat(Name) {
+    const cats_4 = cats.slice(1)
+    return cats_4
+}
